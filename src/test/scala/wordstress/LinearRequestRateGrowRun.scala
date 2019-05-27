6260f407931169d224290f833406f9d927f3f836
Alexey Ragozin <alexey.ragozin@gmail.com>
2019-05-27 11:43:52 +0300

    Tune RPS test

--- src/test/scala/wordstress/LinearRequestRateGrowRun.scala
+++ src/test/scala/wordstress/LinearRequestRateGrowRun.scala
@@ -7,7 +7,7 @@ import scala.concurrent.duration._
 class LinearRequestRateGrowRun extends CommonSimulation {
 
 	  val USER_CAP = Math.ceil(50 * LOAD_FACTOR).asInstanceOf[Int];
-	  val TARGET_RPS = Math.ceil(2 * LOAD_FACTOR).asInstanceOf[Int];
+	  val TARGET_RPS = Math.ceil(1.3 * LOAD_FACTOR).asInstanceOf[Int];
 
 		setUp(
 				scenario("Quick benchmark")
@@ -15,7 +15,7 @@ class LinearRequestRateGrowRun extends CommonSimulation {
 
 					.inject(rampConcurrentUsers(2).to(USER_CAP).during (10 minutes))
 
-					.throttle(jumpToRps(5), reachRps(TARGET_RPS) in (10 minutes))
+					.throttle(jumpToRps(2), reachRps(TARGET_RPS) in (10 minutes))
 
 					.protocols(httpProtocol)
 		).maxDuration(10 minutes).disablePauses
